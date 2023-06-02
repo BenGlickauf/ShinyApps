3cdc2c867befad0ccb53d3e8c3dd184821c835e0
Ben Glickauf <glickaufbm@appstate.edu>
2023-06-02 18:48:58 -0400

    more stuff

--- Classjunk/app.R
+++ Classjunk/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful"),
+    titlePanel("Old Faithfuls"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
